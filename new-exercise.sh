f91b4f2e6ebeda533e102a8644101bd089bfc751
PolpOnline <git@t0mmy.anonaddy.com>
2025-09-23 13:03:16 +0200

    Add new exercise script

---
+++ new-exercise.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# constants
+last_name="Melacarne"
+
+# command line args
+exercise_number=$1
+exercise_name=$2
+
+exercise_dir_name=""$exercise_number"_"$exercise_name""
+
+# create directory
+mkdir -p ./assignments/"$exercise_dir_name"/
+
+# create new directory with template exercise
+rsync -av --progress ./template/ ./assignments/"$exercise_dir_name"/ --exclude mariadb_data
