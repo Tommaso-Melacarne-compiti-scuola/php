b6e5160a29475b710c163bd4bc944b4040dc4190
PolpOnline <git@t0mmy.anonaddy.com>
2025-09-23 18:11:11 +0200

    Fix new exercise script

--- new-exercise.sh
+++ new-exercise.sh
@@ -7,7 +7,7 @@ last_name="Melacarne"
 exercise_number=$1
 exercise_name=$2
 
-exercise_dir_name=""$exercise_number"_"$exercise_name""
+exercise_dir_name="${exercise_number}_${exercise_name}_${last_name}"
 
 # create directory
 mkdir -p ./assignments/"$exercise_dir_name"/
