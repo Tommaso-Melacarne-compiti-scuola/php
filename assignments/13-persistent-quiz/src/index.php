7483fe26d465a793b9025d9db9a889230f6a96f2
PolpOnline <git@t0mmy.anonaddy.com>
2025-11-25 09:43:01 +0100

    Fix quiz style

--- assignments/13-persistent-quiz/src/index.php
+++ assignments/13-persistent-quiz/src/index.php
@@ -8,8 +8,10 @@ session_start(); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
-    <?php if (isset($_SESSION["current_question"])): ?>
-        <title>Quiz: question <?= $_SESSION["current_question"] ?></title>
+    <?php if (isset($_SESSION["quiz_end"])): ?>
+        <title>Quiz: completed</title>
+    <?php elseif (isset($_SESSION["current_question"])): ?>
+        <title>Quiz: question <?= $_SESSION["current_question"] + 1 ?></title>
     <?php else: ?>
         <title>Quiz: start</title>
     <?php endif; ?>
@@ -17,50 +19,54 @@ session_start(); ?>
 <body data-bs-theme="dark">
     <h1 class="text-center mt-3 mb-5">Persistent Quiz</h1>
 
-    <?php if (isset($_SESSION["quiz_end"])): ?>
-        <div class="alert alert-success w-25 mx-auto text-center" role="alert">
-            <h2>Quiz Completed!</h2>
-            <p>Your score: <?= $_SESSION["score"] ?></p>
-            <form action="process_answer.php" method="post">
-                <button type="submit" class="btn btn-success w-100">Restart Quiz</button>
-            </form>
-        </div>
-    <?php elseif (!isset($_SESSION["current_question"])): ?>
-        Welcome to the quiz!<br>
-        <form action="process_answer.php" method="post">
-            <button type="submit">Start Quiz</button>
-        </form>
-    <?php else: ?>
-        <div class="w-50 mx-auto">
-            <?php
-            require_once "quiz_data.php";
+    <div class="d-flex w-100 justify-content-center mx-auto" style="max-width: 500px;">
+        <?php if (isset($_SESSION["quiz_end"])): ?>
+            <div class="alert alert-success text-center w-100 mx-3" role="alert">
+                <h2>Quiz Completed!</h2>
+                <p>Your score: <?= $_SESSION["score"] ?></p>
+                <form action="process_answer.php" method="post">
+                    <button type="submit" class="btn btn-success w-100">Restart Quiz</button>
+                </form>
+            </div>
+        <?php elseif (!isset($_SESSION["current_question"])): ?>
+            <div class="alert alert-info text-center w-100 mx-3" role="alert">
+                <h2>Welcome to the quiz!</h2>
+                <form action="process_answer.php" method="post">
+                    <button type="submit" class="btn btn-primary w-100">Start Quiz</button>
+                </form>
+            </div>
+        <?php else: ?>
+            <div class="w-100 mx-3">
+                <?php
+                require_once "quiz_data.php";
 
-            $current_question_index = $_SESSION["current_question"];
-            $current_question = quiz_data[$current_question_index];
-            ?>
+                $current_question_index = $_SESSION["current_question"];
+                $current_question = quiz_data[$current_question_index];
+                ?>
 
-            <form action="process_answer.php" method="post">
-                <fieldset>
-                    <legend>Question <?= $current_question_index +
-                        1 ?>:</legend>
-                    <p><?= htmlspecialchars(
-                        $current_question["question"],
-                    ) ?></p>
-                    <?php foreach (
-                        $current_question["options"]
-                        as $index => $option
-                    ): ?>
-                        <div class="form-check">
-                            <input type="radio" class="form-check-input" id="option<?= $index ?>" name="answer" value="<?= $index ?>" required>
-                            <label class="form-check-radio" for="option<?= $index ?>">
-                                <?= htmlspecialchars($option) ?>
-                            </label>
-                        </div>
-                    <?php endforeach; ?>
-                </fieldset>
-                <button type="submit" class="btn btn-primary mt-3">Submit Answer</button>
-            </form>
-        </div>
-    <?php endif; ?>
+                <form action="process_answer.php" method="post">
+                    <fieldset>
+                        <legend>Question <?= $current_question_index +
+                            1 ?>:</legend>
+                        <p><?= htmlspecialchars(
+                            $current_question["question"],
+                        ) ?></p>
+                        <?php foreach (
+                            $current_question["options"]
+                            as $index => $option
+                        ): ?>
+                            <div class="form-check">
+                                <input type="radio" class="form-check-input" id="option<?= $index ?>" name="answer" value="<?= $index ?>" required>
+                                <label class="form-check-radio" for="option<?= $index ?>">
+                                    <?= htmlspecialchars($option) ?>
+                                </label>
+                            </div>
+                        <?php endforeach; ?>
+                    </fieldset>
+                    <button type="submit" class="btn btn-primary mt-3">Submit Answer</button>
+                </form>
+            </div>
+        <?php endif; ?>
+    </div>
 </body>
 </html>
